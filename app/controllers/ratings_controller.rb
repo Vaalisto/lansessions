f65f52c038a74f927739a4fa6379ddd8eeac9b56
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-04-22 14:33:03 +0300

    Muokattu rating-kontrolleria

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -25,16 +25,15 @@ class RatingsController < ApplicationController
   # POST /ratings.json
   def create
     @rating = Rating.new(rating_params)
-
-    respond_to do |format|
-      if @rating.save
-        format.html { redirect_to @rating, notice: 'Rating was successfully created.' }
-        format.json { render :show, status: :created, location: @rating }
-      else
-        format.html { render :new }
-        format.json { render json: @rating.errors, status: :unprocessable_entity }
-      end
-    end
+    if current.user.nil?
+      redirect_to signin_path, notice: "Please, sign in"
+    elseif @rating.save
+      current_user.ratings << @rating
+      redirect_to user_path current_user
+    else
+      @games = Game.all
+      render :new
+    end    
   end
 
   # PATCH/PUT /ratings/1
