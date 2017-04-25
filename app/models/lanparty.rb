dc4de3966943d971c9782840ccf9f2d933d0eed1
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-04-25 23:19:16 +0300

    Saatu kalenteri näkymään kunnolla

--- app/models/lanparty.rb
+++ app/models/lanparty.rb
@@ -2,4 +2,12 @@ class Lanparty < ActiveRecord::Base
 	validates :partyname, presence: true
 	validates :address, presence: true
 	validates :city, presence: true
+
+	def start_time
+		self.startdate
+	end
+
+	def end_time
+		self.enddate
+	end
 end
